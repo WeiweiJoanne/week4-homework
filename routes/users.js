b7caa05a37594d3d2e7842f38acc87ee521cc0d5
joannewei <joannewei0921@gmail.com>
2022-05-09 02:20:51 +0800

    update: users router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const UserModel = require('../models/user')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const getUser = UserModel.find({})
+  
+  res.end()
 });
 
 module.exports = router;
