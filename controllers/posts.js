5e5cd8733eaa3649c50446de2b8dbfe43e172ce0
joannewei <joannewei0921@gmail.com>
2022-05-14 00:55:57 +0800

    feat:  posts controllers / posts methods

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,16 +1,72 @@
 const mongoose = require('mongoose')
 
+const handleSuccess = require('../services/handleSuccess')
+const handleError = require('../services/handleError')
+
 const PostModel = require('../models/posts')
+const UserModel = require('../models/user')
 
 const PostsController = {
-  async getPosts(req, res) {
-    const getPosts = await PostModel.find({})
-    res.send({
-      "status": "success",
-      getPosts
+  async getAllPosts(req, res) {
+    const getAllPosts = await PostModel.find({}).populate({
+      path:"user",
+      select: "name photo"
     })
-    res.end()
-  }
+    handleSuccess(res, getAllPosts)
+  },
+  async postPosts(req, res) {
+    const body = req.body
+    const { content, image, user } = body
+    if (content.trim() !== '') {
+      try {
+        const postPosts = await PostModel.create({
+          content, image, user
+        })
+        handleSuccess(res, postPosts)
+      } catch (err) {
+        handleError(res, err)
+      }
+    } else {
+      handleError(res)
+    }
+  },
+  async updatePosts(req, res) {
+    const id = req.params
+    const body = req.body
+    const { content, image, user } = body
+
+    if (content.trim() !== '') {
+      try {
+        const updatePosts = await PostModel.findByIdAndUpdate(
+          id,
+          { content, image, user },
+          { returnDocument: 'after', runValidators: true }
+        )
+        updatePosts !== null ? handleSuccess(res, updatePosts) : handleError(res)
+      } catch (err) {
+        handleError(res, err)
+      }
+    } else {
+      handleError(res)
+    }
+  },
+  async deleteOnePosts(req, res) {
+    const id = req.params
+    try {
+      const deleteOnePosts = await PostModel.deleteOne(id)
+      handleSuccess(res, deleteOnePosts)
+    } catch (err) {
+      handleError(res, err)
+    }
+  },
+  async deleteAllPosts(req, res) {
+    try {
+      const deleteAllPosts = await PostModel.deleteMany({})
+      handleSuccess(res, deleteAllPosts)
+    } catch (err) {
+      handleError(res, err)
+    }
+  },
 }
 
 module.exports = PostsController
--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,13 @@ var router = express.Router();
 const UserController = require('../controllers/users')
 const PostsController = require('../controllers/posts')
 
-
-
 /* GET users listing. */
 router.get('/', UserController.getUser);
 
-// router.get('/posts')
+router.get('/posts', PostsController.getAllPosts);
+router.post('/posts', PostsController.postPosts);
+router.delete('/posts', PostsController.deleteAllPosts);
+router.delete('/posts/:id', PostsController.deleteOnePosts);
+router.patch('/posts/:id', PostsController.updatePosts);
 
 module.exports = router;
